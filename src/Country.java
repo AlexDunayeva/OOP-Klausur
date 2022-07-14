42b2025085697915c202a11cb43def9267789ca9
Alex <ad@thepeaklab.com>
2022-07-14 10:04:48 +0200

    interface

--- src/Country.java
+++ src/Country.java
@@ -1,9 +1,20 @@
-public enum Country {
+public enum Country implements Airport{
     BERLIN,
     LONDON,
     MADRIT,
     PARIS,
     NEW_YORK,
     OSAKA,
-    OSLO
+    OSLO;
+
+    @Override
+    public String generateAirportCode() {
+        char[] code = this.toString().toCharArray();
+        String airportCode = "";
+        for(int i = 0; i<= 2; i++){
+            airportCode += code[i];
+        }
+        return airportCode;
+    }
+
 }
